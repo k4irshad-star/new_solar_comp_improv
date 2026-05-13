efed80f5d62ff2845465c2c1e32c7a69e94637c5
Mohammed Khurram <k.4irshad@gmail.com>
2026-05-13 20:23:19 +0500

    updated code

--- solarcompnew.py
+++ solarcompnew.py
@@ -154,27 +154,31 @@ if voltage_type == "Hybrid (AC & DC)":
     st.markdown("**Power Requirements:**")
     #col1, col2 = st.columns(2)
     with col1:
-        if major_load_type == "MAIN DC LOAD":    
-            power_watts_dc = st.number_input(
-                "🔋 DC Power Requirement (W):",
-                min_value=0,
-                value=product_info_base["default_power_watts"] if major_load_type == "MAIN DC LOAD" else 0,
-                step=100,
-                key="product_power_dc",
-                help="Power consumption when running on DC (battery/solar)",
-                
-                
-            )
+        power_watts_dc = st.number_input(
+            "🔋 DC Power Requirement (W):",
+            min_value=0,
+            value=product_info_base["default_power_watts"] if major_load_type == "MAIN DC LOAD" else 0,
+            step=100,
+            key="product_power_dc",
+            help="Power consumption when running on DC (battery/solar)",
+            
+            
+        )
+        power_watts = power_watts_dc
+        st.write("P1",power_watts_dc)
+        st.write("PF",power_watts)
     with col2: 
-        if major_load_type == "MAIN AC LOAD":    
-            power_watts_ac = st.number_input(
-                "🔌 AC Power Requirement (W):",
-                min_value=0,
-                value=product_info_base["default_power_watts"] if major_load_type == "MAIN AC LOAD" else 0,
-                step=100,
-                key="product_power_ac",
-                help="Power consumption when running on AC (grid/inverter)"
-            )
+        power_watts_ac = st.number_input(
+            "🔌 AC Power Requirement (W):",
+            min_value=0,
+            value=product_info_base["default_power_watts"] if major_load_type == "MAIN AC LOAD" else 0,
+            step=100,
+            key="product_power_ac",
+            help="Power consumption when running on AC (grid/inverter)"
+        )
+        power_watts = power_watts_ac
+        st.write("P12",power_watts_ac)
+        st.write("PF2",power_watts)
     if major_load_type == "MAIN DC LOAD":
         power_watts = power_watts_dc
         
